375b32bf2f6a63333e827bc58d9336a258b31ad2
Babloo JJ <bablu.mailing@gmail.com>
2017-12-30 16:36:43 +0530

    Create app.js

---
+++ app/modules/app.js
@@ -0,0 +1,28 @@
+"use strict";
+angular.module('landing', []);
+angular.module('airbnb', [
+	'ui.router',
+	'landing'
+	])
+
+.controller('rootCtrl', function($scope, $rootScope){
+	var rc = this;
+	$rootScope.title = 'root scope';
+
+})
+
+.config( function($stateProvider, $locationProvider){
+	$locationProvider.html5Mode(true);
+
+	$stateProvider
+	
+	.state('index', {
+		url   : '/',
+		views :  {
+			sectionView: {
+				controller: 'landingCtrl',
+				template : '<h2>Landing section</h2>'
+			}
+		}
+	}) 
+})
